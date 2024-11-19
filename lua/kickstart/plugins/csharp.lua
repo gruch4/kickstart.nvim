a3f778b477b94aa29022c64009dc6bf2505349d6
Michal Gruszczynski <gruch4@gmail.com>
2024-11-19 20:12:07 +0100

    Commented out keymap in csharp

--- lua/kickstart/plugins/csharp.lua
+++ lua/kickstart/plugins/csharp.lua
@@ -15,7 +15,7 @@ return {
   end,
 
   -- vim.keymap.set('n', '<leader>csr', '<cmd>lua require("csharp").run_project()<cr>'),
-  vim.keymap.set('n', '<leader>csr', function()
-    require('csharp').run_project()
-  end),
+  --vim.keymap.set('n', '<leader>csr', function()
+  --  require('csharp').run_project()
+  -- end),
 }
